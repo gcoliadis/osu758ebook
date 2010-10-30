96a37d5e4f098bb16a76571b5022024b9f228c90
Gus Coliadis <gcoliadis@gmail.com>
2010-10-29 23:48:16 -0400

    textbook

---
+++ textbook/Classes/textbookAppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  textbookAppDelegate.h
+//  textbook
+//
+//  Created by gcoliadi on 10/24/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface textbookAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+	UIWebView *webView;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UIWebView *webView;
+
+@end
+
---
+++ textbook/Classes/textbookAppDelegate.m
@@ -0,0 +1,79 @@
+//
+//  textbookAppDelegate.m
+//  textbook
+//
+//  Created by gcoliadi on 10/24/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "textbookAppDelegate.h"
+
+@implementation textbookAppDelegate
+
+@synthesize window;
+@synthesize webView;
+
+
+#pragma mark -
+#pragma mark Application lifecycle
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+    
+	
+	// Override point for customization after application launch.
+    
+    [window makeKeyAndVisible];
+	[webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://reddit.com"]]];
+	
+	//diasble Scrolling "Bounce"
+	for (id subview in webView.subviews)
+		if ([[subview class] isSubclassOfClass: [UIScrollView class]])
+			((UIScrollView *)subview).bounces = NO;
+	
+    return YES;
+}
+
+// WebView is finished loading
+
+
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    /*
+     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+     */
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    /*
+     Restart any tasks that were paused (or not yet started) while the application was inactive.
+     */
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    /*
+     Called when the application is about to terminate.
+     */
+}
+
+
+#pragma mark -
+#pragma mark Memory management
+
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
+    /*
+     Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
+     */
+}
+
+
+- (void)dealloc {
+    [window release];
+    [super dealloc];
+}
+
+
+@end
